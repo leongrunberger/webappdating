3ae65a1df667f150352716a12960063580c5be02
roflcopter600000 <koch.pascal_95@web.de>
2020-02-12 15:46:23 +0100

    Installed Vuejs

---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,37 @@
+   <template>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Vuejs  feat. Laravel - Tinder 2.0 y
+                </div>
+            
+            </div>
+        </div>
+    </template>
+    <script>
+        export default {}
+    </script>
+    <style scoped>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+    </style>
