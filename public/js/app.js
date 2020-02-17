63c28421a793f1e6d52bd8dccec3f9475be41372
leongrunberger <leongrunberger@web.de>
2020-02-17 11:40:10 +0100

    Login und Registration hinzugefügt
    Aufsetzen neues Laravel-Projekts + Login/Registrierung + Vue.js Installation

--- public/js/app.js
+++ public/js/app.js
@@ -50105,8 +50105,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\webappdatingParent\webappdating\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\webappdatingParent\webappdating\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\testweb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\testweb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
