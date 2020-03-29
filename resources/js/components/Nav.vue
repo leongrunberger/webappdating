31e550bfc7c835d03e39f38a2cfe8bdd56fac343
Marius <marius.staud@icloud.com>
2020-03-29 17:18:50 +0200

    Neue Startseite

--- resources/js/components/Nav.vue
+++ resources/js/components/Nav.vue
@@ -1,30 +1,8 @@
-<template>
-    <div class="container">
-        <div class="row">
-            <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Active</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</template>
+<template></template>
 
 <script>
 export default {};
-
-
 </script>
 
 <style scoped>
-
 </style>
--- resources/js/components/Welcome.vue
+++ resources/js/components/Welcome.vue
@@ -1,27 +1,19 @@
 <template>
-    <div class="container">
-        <div class="alert alert-primary" role="alert">
-            Achtung! Diese App ist extrem nice. Ehefrauen hassen diese App.
-        </div>
-
-        <div class="row">
-            <div class="jumbotron">
-                <h1 class="display-4">Bootstrap Jumbotron</h1>
-                <p class="lead">Jetzt wird geswipet</p>
-                <hr class="my-4">
-                <p>Hallöchen</p>
-                <a class="btn btn-primary btn-lg" href="#" role="button">Jetzt komplett abswipen</a>
-            </div>
-        </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="jumbotron">
+        <h1 class="display-4">Willkommen bei Flankr!</h1>
+        <p class="lead">Flankr ist die beste Dating-App auf dem Markt.</p>
+        <a class="btn btn-primary" href="/login" role="button">Login</a>
+        <a class="btn btn-secondary" href="/register" role="button">Registrieren</a>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {};
-
-
 </script>
 
 <style scoped>
-
 </style>
