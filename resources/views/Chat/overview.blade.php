8822c4a8554c0cf3e7ae0ef6f1a711983fac45f9
TimKustermann <T.Kusterman@gmx.de>
2020-03-30 14:35:28 +0200

    Chatfunktion Seite anlegen Schritt 1

--- resources/views/Chat/overview.blade.php
+++ resources/views/Chat/overview.blade.php
@@ -1,23 +1,112 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="user-wrapper">
+                <ul class="users">
+                    <li class="users">
+                        <span class="pending">1</span>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+                            
+                            <div class="media-body">
+                                <p class="name">Johne Doe</p>
+                                <p class ="email">john@gmail.com</p>
+                            </div>
                         </div>
-                    @endif
+                    </li>
 
-                    You are logged in!
-                </div>
+                    <li class="users">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+                            
+                            <div class="media-body">
+                                <p class="name">Johne Doe</p>
+                                <p class ="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+
+                    <li class="users">
+                        <span class="pending">1</span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+                            
+                            <div class="media-body">
+                                <p class="name">Johne Doe</p>
+                                <p class ="email">john@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
-    </div>
+
+        <div class="col-md-8" id="messages">
+            <div class="message-wrapper">
+                <ul class="messages">
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor.</p>
+                            <p class="date">6 Apr, 2020</p>
+                        </div>
+                    </li>
+
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor.</p>
+                            <p class="date">6 Apr, 2020</p>
+                        </div>
+                    </li>
+
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor.</p>
+                            <p class="date">6 Apr, 2020</p>
+                        </div>
+                    </li>
+
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor.</p>
+                            <p class="date">6 Apr, 2020</p>
+                        </div>
+                    </li>
+
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor.</p>
+                            <p class="date">6 Apr, 2020</p>
+                        </div>
+                    </li>
+
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor.</p>
+                            <p class="date">6 Apr, 2020</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="input-text">
+                <input type="text"name="message" class="submit">
+            </div>
+            
+        </div>
+
+    </div>                    
 </div>
 @endsection
