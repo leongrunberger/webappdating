ab9562991c7e73f06f3f42dbb1020eb28702e2b4
TimKustermann <T.Kusterman@gmx.de>
2020-03-30 18:24:20 +0200

    Chatfunktion: Layout Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,15 @@
 
     <!-- Style Chat -->
     <style>
+
+        ul {
+            margin: 0;
+            padding: 0;
+        }
+
+        li {
+            list-style: none; 
+        }
         .user-wrapper, .message-wrapper {
             border: 1px solid #dddddd;
             overflow-y: auto;
@@ -45,9 +54,7 @@
         }
 
        .pending {
-           position: absolute;
-           left: 13px;
-           
+           position: absolute;  
            background:#b600ff;
            margin: 0;
            border-radius: 50%;
@@ -56,9 +63,77 @@
            padding-left: 5px;
            color:  #ffffff;
            font-size: 12px; 
+       }
 
+       .media-left {
+           margin: 0 10px;
+       }
+
+       .media-left img {
+           width: 64px;
+           border-radius: 64px;
+       }
+
+       .media-body p {
+           padding: 6px 0; 
        }
      
+       .message-wrapper {
+           padding: 10px;
+           height: 536px;
+           background: #eeeeee;
+       }
+
+       .messages .message {
+           margin-bottom: 15px;
+       }
+
+       .messages .message:last-child {
+           margin-bottom: 0;
+       }
+
+       .received, sent {
+           width: 45%;
+           padding: 3px 10px;
+           border-radius: 10px;
+       }
+
+       .received {
+           background: #ffffff;
+       }
+
+       .sent {
+           background: #3bebff;
+           float: right;
+           text-align: right;
+       }
+
+       .message p {
+           margin: 5px 0;
+       }
+
+       .date {
+           color: #777777;
+           font-size: 12px;
+       }
+
+       .active {
+           background: #eeeeee;
+       }
+
+       input [type=text] {
+           width: 100%;
+           padding: 12px 20px;
+           margin: 15px 0 0 0;
+           display: inline-block;
+           border-radius: 4px;
+           box-sizing: border-box;
+           outline: none;
+           border: 1px solid #cccccc;
+       }
+       input [type=text]:focus {
+           border: 1px solid #aaaaaa;
+       }
     </style>
 
 </head>
