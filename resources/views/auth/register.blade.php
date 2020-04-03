1153d44c77621e254cf4cca96109ee4b206879e3
leongrunberger <leongrunberger@web.de>
2020-04-03 13:18:49 +0200

    Login und Register farblich angepasst
    Layout der Login und Register Funktion dem Flankr layout angepasst

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card mt-4 flankr-profile-card">
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
@@ -99,7 +99,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-light flankr-button">
                                     {{ __('Register') }}
                                 </button>
                             </div>
