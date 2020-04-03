b150b96f6674773aba4a2f23be96222f6b7fa28d
Marius <marius.staud@icloud.com>
2020-04-03 12:40:06 +0200

    Menüleiste: Finale Änderung

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -174,19 +174,19 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item">
-                                <a class="nav-link flankr-grey-70" href="/chats">Chats</a>
-                            </li>
-                            <li class="nav-item mr-5">
-                                <a class="nav-link flankr-grey-70" href="/profile">Profil</a>
-                            </li>
-
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle flankr-grey-70" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}<span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right flankr-grey-20-bg" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item flankr-grey-70" href="/start">Start
+                                    </a>
+                                    <a class="dropdown-item flankr-grey-70" href="/profile">Profil
+                                    </a>
+                                    <a class="dropdown-item flankr-grey-70" href="/chats">Chats
+                                    </a>
+                                    <div class="dropdown-divider flankr-grey-70"></div>
                                     <a class="dropdown-item flankr-grey-70" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
