2cdcbd9d7f36ec0f23b1eac862e125c5b19c78cf
leongrunberger <leongrunberger@web.de>
2020-04-03 12:32:50 +0200

    Anpassen Layout
    Bilder begrenzen

--- resources/views/Start/index.blade.php
+++ resources/views/Start/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-4">
             <h1>Deine Beute</h1>
             @if(count($profiles) > 1)
                
